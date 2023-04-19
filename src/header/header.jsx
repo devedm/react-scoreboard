ae56f6d83157a8d1cae06f829c7efb24387cb79b
Eddy Mena <developer.eddymena@gmail.com>
2023-04-19 03:55:34 -0600

    added header component

---
+++ src/header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./logo.svg" alt="FIFA World Cup Scoreboard" />
+      <h2>FIFA World Cup Scoreboard</h2>
+    </header>
+  );
+}
